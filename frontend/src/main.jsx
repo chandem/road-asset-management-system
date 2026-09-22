000f029f9e030e30d655bb64e1ff30ebe02eb3fb
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2026-09-22 12:06:42 +0300

    style: import offline-ui stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import "./kpi.css";
+import "./offline-ui.css";
 import AuthGate from "./AuthGate";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
